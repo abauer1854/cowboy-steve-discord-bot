8bd99a14ede3605b4158ac7fa8f2de5b43f544c7
abauer1854 <abauer2@umbc.edu>
2024-03-04 16:37:19 -0500

    begin implementing image reading

--- bot.py
+++ bot.py
@@ -1,10 +1,9 @@
 import discord
 import responses
 
-# eoieoeo
-async def send_message(message, user_message, is_private):
+async def send_message(message, user_message, image, is_private):
     try:
-        response = responses.handle_response(user_message)
+        response = responses.handle_response(user_message, image)
         await message.author.send(response) if is_private else await message.channel.send(response)
     except Exception as e:
         print(e)
@@ -25,21 +24,33 @@ def run_bot():
         print(f'{client.user} is now running!')
     
     @client.event
+
     async def on_message(message):
         if message.author == client.user:
             return
         
+        image = None
         username = str(message.author)
         user_message = str(message.content)
         channel = str(message.channel)
 
+        # handling images
+        if message.attachments:
+            image = message.attachments # if there are multiple attachments, image becomes a list of attachment objects
+        
+        print(user_message, "\n", image)
+
         print(f"{username} said: '{user_message}' ({channel})")
 
-        if user_message[0] == '?':
-            user_message = user_message[1:]
-            await send_message(message, user_message, is_private=True)
-        else:
-            await send_message(message, user_message, is_private=False)
+        # if user_message:
+        #     if user_message[0] == '?':
+        #         user_message = user_message[1:]
+        #         await send_message(message, user_message, is_private=True)
+        #     else:
+        #         await send_message(message, user_message, is_private=False)
+
+        await send_message(message, user_message, image, is_private=False)
+
 
 
     client.run(TOKEN)
--- responses.py
+++ responses.py
@@ -1,21 +1,68 @@
 import os
 import google.generativeai as genai
+from PIL import Image # pillow, python imaging library
+import requests
 
 # https://codemaker2016.medium.com/build-your-own-chatgpt-using-google-gemini-api-1b079f6a8415
 # https://ai.google.dev/
 # https://github.com/DeSinc/SallyBot
+# https://www.analyticsvidhya.com/blog/2023/12/google-gemini-api/
+# https://pillow.readthedocs.io/en/stable/
 
 genai.configure(api_key = os.environ['GOOGLE_API_KEY'])
-model = genai.GenerativeModel('gemini-pro')
+MODEL = genai.GenerativeModel('gemini-pro')
 
-def handle_response(message) -> str:
-    p_message = message.lower()
+
+# collection of one-liner responses that honduras makes when specific words/phrases are said
+def honduras_interruptions(message):
     interruption = ""
-
     if 'honduras' in message:
         interruption += "whart\n"
     
     if 'youtube' in message:
         interruption += 'I love youtube\n'
+    
+    return interruption
 
-    return interruption + model.generate_content(p_message).text
+# convert discord attachment url to images using PIL
+def convert_url_to_image(image_url):
+    if not image_url:
+        return image_url
+    return Image.open(requests.get(image_url, stream=True).raw)
+    
+
+def handle_response(message, image_url) -> str:
+    image = convert_url_to_image(image_url)
+    p_message = message.lower()
+    interruption = honduras_interruptions(p_message)
+
+    ai_msg = MODEL.generate_content(p_message,
+                                    safety_settings=[
+                                                    {
+                                                        "category": "HARM_CATEGORY_HARASSMENT",
+                                                        "threshold": "BLOCK_NONE",
+                                                    },
+                                                    {
+                                                        "category": "HARM_CATEGORY_HATE_SPEECH",
+                                                        "threshold": "BLOCK_NONE",
+                                                    },
+                                                    {
+                                                        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+                                                        "threshold": "BLOCK_NONE",
+                                                    },
+                                                    {
+                                                        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+                                                        "threshold": "BLOCK_NONE",
+                                                    }
+                                                ],
+                                    generation_config = genai.types.GenerationConfig(
+                                    candidate_count = 1,
+                                    stop_sequences = [],
+                                    max_output_tokens = 50, # a token is approximately 4 characters so 160 char output roughly
+                                    top_p = 0.9,
+                                    top_k = 5,
+                                    temperature = 1.0
+                                    ))
+    print(ai_msg.prompt_feedback)
+
+    return interruption + ai_msg.text
